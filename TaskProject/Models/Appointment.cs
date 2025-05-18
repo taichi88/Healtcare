71041fd521b830969fdb1200bac070b3bd3979c0
taichi88 <nikoloz.gabeskiria@gmail.com>
2025-05-18 12:22:57 +0400

    Models Creation

---
+++ TaskProject/Models/Appointment.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskProject.Models
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+        public string ReasonForVisit { get; set; }
+
+        public Patient Patient { get; set; }
+        public Doctor Doctor { get; set; }
+    }
+
+}
---
+++ TaskProject/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskProject.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+
+}
