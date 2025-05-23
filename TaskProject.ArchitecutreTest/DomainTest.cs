2dff87bc8622df96c9a667680652004bcc5e75f7
taichi88 <nikoloz.gabeskiria@gmail.com>
2025-05-23 15:55:05 +0400

    Added ArchTestPackages + regenereated migrations + testing

---
+++ TaskProject.ArchitecutreTest/DomainTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace TaskProject.ArchitecutreTest
+{
+    public class DomainTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
